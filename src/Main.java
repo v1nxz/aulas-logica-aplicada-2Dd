7a66254a7160281e1e5d2eb43e51490a84000926
v1nxz <vinijbarreto@gmail.com>
2023-05-08 12:10:17 -0300

    Apaguei Tudo

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // laço enquanto - while
-        int contador = 0;
-        while(contador < 10){
-            // processos
-            System.out.println("Contador = " + contador);
-            contador = contador + 1; //incremento
-        }
-        // laço faça enquanto - do while
-        int cont = 1;
-        do{
-            System.out.println(cont);
-            cont++; //incremento
-        }while(cont <= 10);
-
-        // laço para faça for
-        for(int x=0; x<10; x++){
-            System.out.println(x);
-        }
+        // Aula inicial de Orientação e Objetos
     }
 }
